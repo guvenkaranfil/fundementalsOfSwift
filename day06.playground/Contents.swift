f0ddbb2944b33b3eaf4ce9ac8da75c0886fa4303
guvenkaranfil <guvenkaranfil235@gmail.com>
2020-12-06 22:54:33 +0300

    feat: day06 completed

---
+++ day06.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+
+let welcomeMessage = { (name: String) in
+    print("Welcome \(name)")
+}
+
+// as seen below, closures don't use paramater labels like normal functions while running the function
+welcomeMessage("güven")
+
+
+let sayHiWithReturn = { (name: String) -> String in
+    return "Hi Admin " + name
+}
+
+let message = sayHiWithReturn("Guven")
+print(message)
+
+
+// closures as paramaters
+let driving = {
+    print("I'm driving in my car")
+}
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
+
+print ("--------------------------------")
+
+// trailing closure syntax
+func travel2(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel2 {
+    print("I'm driving in my car")
+}
